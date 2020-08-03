c0b7f62efa60ddb7c87816dd9b31147fffb9677c
Alireza <alireza.hashemi13@outlook.com>
2020-08-04 01:35:22 +0430

    better error handling

--- get_movies.py
+++ get_movies.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from time import sleep
 
 base_url = 'https://letterboxd.com/films/by/rating/size/small/page/'
-page = 1
+page = 80
 
 while(True):
     print("Page:",page)
@@ -17,33 +17,37 @@ while(True):
         response = session.get(url)
     except requests.exceptions.RequestException as e:
         print(e)
+    while(True):
+        try:
+            response.html.render()
 
-    try:
-        response.html.render()
-    except:
-        print('Error in rendering, retrying...')
-        sleep(5)
+            html = response.html.raw_html
 
-    html = response.html.raw_html
-
-    soup = BeautifulSoup(html, 'html.parser')
-    content = soup.find(id='content')
-    content = content.find(class_='content-wrap')
-    content = content.find(class_='section col-24 col-main')
-    content = content.find(id='films-browser-list-container')
-    content = content.find(class_='poster-list -p70 -grid')
-    
-    items = content.find_all(class_='listitem poster-container')
-    items = [item.find_all(class_='')[0].find(class_='frame') for item in items]
+            soup = BeautifulSoup(html, 'html.parser')
+            content = soup.find(id='content')
+            content = content.find(class_='content-wrap')
+            content = content.find(class_='section col-24 col-main')
+            content = content.find(id='films-browser-list-container')
+            content = content.find(class_='poster-list -p70 -grid')
+            
+            items = content.find_all(class_='listitem poster-container')
+            items = [item.find_all(class_='')[0].find(class_='frame') for item in items]
+        except:
+            print('Error in rendering, retrying...')
+            sleep(2)
+            continue
+        break
 
     names = []
     links = []
+    years = []
     for i in tqdm(range(len(items))):
         attrs = items[i].attrs
         name_ = attrs['data-original-title'].split(sep=' ')
         names.append(' '.join(name_[:-1]))
         links.append('https://letterboxd.com'+attrs['href'][:-1])
-
+        years.append(name_[-1][1:-1])
+    
     with open('movies.txt','a') as f:
         for i in range(len(names)):
-            f.writelines(names[i]+','+links[i]+'\n')
+            f.writelines(names[i]+','+years[i]+','+links[i]+'\n')
